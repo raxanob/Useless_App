864acb55232387fff86ee2ec58c37041a96cee34
raxanob <rayanexavier161@gmail.com>
2020-04-15 18:15:38 -0300

    Project creation

---
+++ Useless_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Useless_App
+//
+//  Created by Rayane Xavier on 15/04/20.
+//  Copyright © 2020 Rayane Xavier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
